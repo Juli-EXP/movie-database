25088efc627270858f1198cd349391dabfcf9590
Juli-EXP <juli.lamprecht@gmail.com>
2021-05-25 20:45:56 +0200

    Bugfix

--- client/src/components/Rating/RatingCard.js
+++ client/src/components/Rating/RatingCard.js
@@ -7,10 +7,9 @@ const RatingCard = ({rating}) => {
         <div className={"w-96 md: p-2 bg-primary hover:bg-secondary"}>
             <p className={"font-bold"}>{rating.username}</p>
             <p>{rating.comment}</p>
-            <p>{rating.rating}</p>
             <span className={"text-white text-2xl flex flex-row"}>
                 <RatingStar className={"m-0.5 "}/>
-                <p>{Number((movie.rating || 0.0).toFixed(1))}</p>
+                <p>{Number((rating.rating || 0.0).toFixed(1))}</p>
             </span>
 
         </div>
